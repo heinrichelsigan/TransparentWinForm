efc195762de8a4387d388e482bf9b22174397534
Heinrich Elsigan <heinrich.elsigan@gmail.com>
2022-08-19 06:45:48 +0200

    1st commit

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,52 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.OnLoad);
+            this.ResizeEnd += new System.EventHandler(this.OnResizeEnd);
+            this.LocationChanged += new System.EventHandler(this.OnLocationChanged);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,81 @@
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        protected Image? winDeskImg = null;
+        volatile bool locChangedOff = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        public void SetTransBG()
+        {
+            if (winDeskImg == null)
+                winDeskImg = GetDesktopImage();
+             
+            Graphics g = Graphics.FromImage(winDeskImg);
+            Form f = this.FindForm();
+            Image? bgImg = Crop(winDeskImg, DesktopBounds.Size.Width, DesktopBounds.Size.Height, f.DesktopBounds.Location.X + 8, f.DesktopBounds.Location.Y + 32);
+            if (bgImg != null)
+                this.BackgroundImage = bgImg;
+        }
+
+        public Image? Crop(Image image, int width, int height, int x, int y)
+        {
+            try
+            {                
+                Bitmap bmp = new Bitmap(width, height, PixelFormat.Format24bppRgb);
+
+                Graphics g = Graphics.FromImage(bmp);
+                g.SmoothingMode = SmoothingMode.AntiAlias;
+                g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                g.PixelOffsetMode = PixelOffsetMode.HighQuality;
+                g.DrawImage(image, new Rectangle(0, 0, width, height), x, y, width, height, GraphicsUnit.Pixel);
+                g.Dispose();
+
+                return bmp;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return null;
+            }
+        }
+
+
+        public Image GetDesktopImage()
+        {
+            locChangedOff = true;
+            this.WindowState = FormWindowState.Minimized;
+            ScreenCapture sc = new ScreenCapture();
+            Image winDeskImg = sc.CaptureScreen();
+            this.WindowState = FormWindowState.Normal;
+            locChangedOff = false;
+            return winDeskImg;
+        }
+
+        private void OnLoad(object sender, EventArgs e)
+        {
+            if (this.winDeskImg == null)
+                SetTransBG();
+        }
+
+        private void OnResizeEnd(object sender, EventArgs e)
+        {
+            winDeskImg = GetDesktopImage();
+            SetTransBG();
+        }
+
+        private void OnLocationChanged(object sender, EventArgs e)
+        {
+            if (!locChangedOff)
+                SetTransBG();
+        }
+    }
+}
